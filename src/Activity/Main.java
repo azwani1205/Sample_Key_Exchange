53bc5a4e71ec66fcc00231947be4cf49afb4016c
user <user@ZAMRI>
2017-11-12 01:59:44 +0800

    new commit

---
+++ src/Activity/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Activity;
+
+/**
+ *
+ * @author ZAMRI
+ */
+public class Main {
+
+    public static void main(final String[] args) throws Exception {
+
+        new Main().init();
+    }
+
+    private void init() {
+        
+        final Person alice = new Person();
+        final Person bob   = new Person();
+
+        alice.generateKeys();
+        bob.generateKeys();
+
+        alice.receivePublicKeyFrom(bob);
+        bob.receivePublicKeyFrom(alice);
+
+        alice.generateCommonSecretKey();
+        bob.generateCommonSecretKey();
+
+        alice.encryptAndSendMessage("Bob! Guess Who I am.", bob);
+
+        bob.whisperTheSecretMessage();
+
+    }
+}
+
